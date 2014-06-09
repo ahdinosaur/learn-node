acc51b9209e8bdc96bf6ad08a8806e14b6e05407
Michael Williams <dinosaur@riseup.net>
2014-06-09 01:30:34 -0700

    prepare next section

--- intro/01-simple/parseStuff.js
+++ intro/01-simple/parseStuff.js
@@ -2,4 +2,14 @@ var fs = require('fs')
 
 var stuff = fs.readFileSync(__dirname + '/stuff.txt').toString()
 
-console.log(stuff)
+var result = []
+
+stuff.split('\n').forEach(function (line) {
+  if (line.charAt(0) === ' ') {
+    console.log(line)
+  }
+
+  result.push(line)
+});
+
+console.log("result", result)
