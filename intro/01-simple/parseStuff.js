eed31ab89d9eecff48524e05ad9bc1b2cc9ba2e7
Michael Williams <dinosaur@riseup.net>
2014-06-09 01:32:00 -0700

    Revert "prepare next section"
    This reverts commit acc51b9209e8bdc96bf6ad08a8806e14b6e05407.

--- intro/01-simple/parseStuff.js
+++ intro/01-simple/parseStuff.js
@@ -2,14 +2,4 @@ var fs = require('fs')
 
 var stuff = fs.readFileSync(__dirname + '/stuff.txt').toString()
 
-var result = []
-
-stuff.split('\n').forEach(function (line) {
-  if (line.charAt(0) === ' ') {
-    console.log(line)
-  }
-
-  result.push(line)
-});
-
-console.log("result", result)
+console.log(stuff)
